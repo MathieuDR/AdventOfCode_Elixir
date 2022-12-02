d3add01401dcd854d83b6f9bfd6b323abc7b8fd9
Mathieu De Raedt <mathieu@deraedt.dev>
2022-12-02 14:23:14 +0100

    specs

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -1,16 +1,19 @@
 defmodule AdventOfCode.Day02 do
+  @spec part1(any) :: integer
   def part1(_args) do
     get_match_lines()
     |> Enum.map(&score_match/1)
     |> Enum.sum()
   end
 
+  @spec part2(any) :: integer
   def part2(_args) do
     get_match_lines()
     |> Enum.map(&decide_and_score_match/1)
     |> Enum.sum()
   end
 
+  @spec decide_and_score_match(binary) :: integer
   def decide_and_score_match(line) do
     [opponent, match] = line
     |> String.split(" ")
@@ -19,9 +22,9 @@ defmodule AdventOfCode.Day02 do
     match_result = score_result(match)
 
     get_shape_score(match_result, opponent_shape) + match_result
-
   end
 
+  @spec get_shape_score(0 | 3 | 6, integer) :: integer
   def get_shape_score(match_result, opponent_shape) do
     case match_result do
       0 -> opponent_shape - 1
@@ -31,10 +34,7 @@ defmodule AdventOfCode.Day02 do
     |> keep_shape_in_bound()
   end
 
-  def keep_shape_in_bound(0), do: 3
-  def keep_shape_in_bound(4), do: 1
-  def keep_shape_in_bound(shape), do: shape
-
+  @spec score_match(binary) :: integer
   def score_match(line) do
     [p1, p2] = line
     |> String.split(" ")
@@ -49,19 +49,26 @@ defmodule AdventOfCode.Day02 do
     |> String.split("\n")
   end
 
-  defp score_result("X"), do: 0
-  defp score_result("Y"), do: 3
-  defp score_result("Z"), do: 6
+  #Make sure that our shapers are still in bound (1 - 3)
+  @spec keep_shape_in_bound(integer) :: integer
+  defp keep_shape_in_bound(0), do: 3
+  defp keep_shape_in_bound(4), do: 1
+  defp keep_shape_in_bound(shape), do: shape
 
-  defp score_shape("Y"), do: 2
-  defp score_shape("X"), do: 1
-  defp score_shape("Z"), do: 3
-  defp score_shape("A"), do: 1
-  defp score_shape("B"), do: 2
-  defp score_shape("C"), do: 3
 
-  defp score_result(1, 3), do: 0
-  defp score_result(3, 1), do: 6
+  defp score_result("X"), do: 0 #LOSE
+  defp score_result("Y"), do: 3 #DRAW
+  defp score_result("Z"), do: 6 #WIN
+
+  defp score_shape("Y"), do: 2 #PAPER
+  defp score_shape("X"), do: 1 #ROCK
+  defp score_shape("Z"), do: 3 #SCISSORS
+  defp score_shape("A"), do: 1 #ROCK
+  defp score_shape("B"), do: 2 #PAPER
+  defp score_shape("C"), do: 3 #SCISSORS
+
+  defp score_result(1, 3), do: 0 # ROCK, SCISSORS
+  defp score_result(3, 1), do: 6 # SCISSORS, ROCK
   defp score_result(p1, p2) when p1 == p2,  do: 3
   defp score_result(p1, p2) when p1 > p2,  do: 0
   defp score_result(p1, p2) when p1 < p2,  do: 6
