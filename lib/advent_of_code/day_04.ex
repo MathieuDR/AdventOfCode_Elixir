db1a0b8314fc617e72ed3ddc87eedddf3d80d523
Mathieu De Raedt <mathieu@deraedt.dev>
2022-12-04 14:29:18 +0100

    part 2

--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -1,16 +1,21 @@
 defmodule AdventOfCode.Day04 do
   def part1(_args) do
-    get_elves_ranges()
-    |> Enum.map(&Enum.map(&1, fn r -> create_range(r) end))
-    |> Enum.map(&has_overlap?/1)
+    get_ranges()
+    |> Enum.map(&overlaps?/1)
     |> Enum.reduce(0, fn overlap, acc -> if overlap do acc + 1 else acc end end)
   end
 
   def part2(_args) do
+    get_ranges()
+    |> Enum.map(&has_overlap?/1)
+    |> Enum.reduce(0, fn overlap, acc -> if overlap do acc + 1 else acc end end)
   end
 
-  def has_overlap?([r1_min..r1_max, r2_min..r2_max] = _) when r1_min <= r2_min and r1_max >= r2_max, do: true
-  def has_overlap?([r1_min..r1_max, r2_min..r2_max] = _) when r1_min >= r2_min and r1_max <= r2_max, do: true
+  def overlaps?([r1_min..r1_max, r2_min..r2_max] = _) when r1_min <= r2_min and r1_max >= r2_max, do: true
+  def overlaps?([r1_min..r1_max, r2_min..r2_max] = _) when r1_min >= r2_min and r1_max <= r2_max, do: true
+  def overlaps?(_) , do: false
+
+  def has_overlap?([r1_min..r1_max, r2_min..r2_max] = _) when r1_min <= r2_max and r1_max >= r2_min, do: true
   def has_overlap?(_) , do: false
 
   def create_range(range) do
@@ -20,10 +25,11 @@ defmodule AdventOfCode.Day04 do
     a..b
   end
 
-  defp get_elves_ranges() do
+  defp get_ranges() do
     AdventOfCode.Input.get!(4, 2022)
     |> String.trim()
     |> String.split("\n")
     |> Enum.map(&String.split(&1, ","))
+    |> Enum.map(&Enum.map(&1, fn r -> create_range(r) end))
   end
 end
