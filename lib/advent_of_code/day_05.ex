c373c97e28b7fd6497ef856b215dafc4209dd40c
Mathieu De Raedt <mathieu@deraedt.dev>
2022-12-06 00:18:43 +0100

    part 2

--- lib/advent_of_code/day_05.ex
+++ lib/advent_of_code/day_05.ex
@@ -1,48 +1,44 @@
 defmodule AdventOfCode.Day05 do
   def part1(_args) do
-    {stacks, moves} = get_planning()
-
-    # IO.inspect(stacks)
-
-    moves
-    |> Enum.reduce(stacks, fn  (%{crates: amount, from: a, to: b} = map, acc) ->
-      IO.inspect(map)
-
-       move_crate(acc, amount, a, b)
-       |> IO.inspect()
-    end)
+    solve()
   end
 
   def part2(_args) do
+    solve(false)
   end
 
-  def move_crate(crates, amount, from, to) do
+  def solve(reverse \\ true) do
+    {stacks, moves} = get_planning()
+
+    moves
+    |> Enum.reduce(stacks, fn  (%{crates: amount, from: a, to: b} = _, acc) ->
+       move_crate(acc, amount, a, b, reverse)
+    end)
+    |> Enum.reduce('', fn (element,acc) ->  append_to_stack(acc, Enum.at(element, 0)) end)
+    |> Enum.reverse()
+  end
+
+  def move_crate(crates, amount, from, to, reverse) do
     from_stack = Enum.at(crates, from - 1)
     to_stack = Enum.at(crates, to - 1)
 
-    {taken, new_from_stack} = remove_x_crates(from_stack, amount)
+    {taken, new_from_stack} = remove_x_crates(from_stack, amount, reverse)
     new_to_stack = append_to_stack(to_stack, taken)
 
     crates
     |> List.replace_at(from - 1, new_from_stack)
     |> List.replace_at(to - 1, new_to_stack)
-
-    # crates
-    # |> Enum.map(fn
-    #   ^from_stack = _ -> new_from_stack
-    #   ^to_stack = _ -> new_to_stack
-    #   s -> s
-    # end)
   end
 
   def append_to_stack(crates, [_|_] = stack), do: stack ++ crates
   def append_to_stack(crates, single), do: [single | crates]
 
-  def remove_x_crates(crate_stack, x) do
+  def remove_x_crates(crate_stack, x, reverse) do
     {removed, tail} = crate_stack
     |> Enum.split(x)
 
-    {Enum.reverse(removed), tail}
+    removed = if reverse, do: Enum.reverse(removed), else: removed
+    {removed, tail}
   end
 
   def get_planning() do
