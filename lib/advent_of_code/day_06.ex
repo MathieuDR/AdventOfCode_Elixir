816ff78f981cf05e6836f77f20f5a5677b7ae2de
Mathieu De Raedt <mathieu@deraedt.dev>
2022-12-08 15:53:23 +0100

    day 6

--- lib/advent_of_code/day_06.ex
+++ lib/advent_of_code/day_06.ex
@@ -1,7 +1,38 @@
 defmodule AdventOfCode.Day06 do
   def part1(_args) do
+    get_data_stream()
+    |> IO.inspect()
+    |> search_first_packet(4)
   end
 
   def part2(_args) do
+    get_data_stream()
+    |> IO.inspect()
+    |> search_first_packet(14)
+  end
+
+  def search_first_packet([_ | tail] = stream, amount, counter \\ 0) do
+    {split, _} = Enum.split(stream, amount)
+
+    if check_repeating(split) do
+      search_first_packet(tail, amount, counter + 1)
+    else
+      counter + amount
+    end
+  end
+
+
+  def check_repeating([] = _), do: false
+  def check_repeating([c | tail] = _) do
+    # IO.inspect(c)
+    # IO.inspect(tail)
+    if Enum.member?(tail, c), do: true, else: check_repeating(tail)
+  end
+
+  def get_data_stream() do
+    AdventOfCode.Input.get!(6, 2022)
+    |> String.trim()
+    # |> String.split("\n")
+    |> String.to_charlist()
   end
 end
